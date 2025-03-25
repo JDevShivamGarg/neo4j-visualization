8c9ee060ae1b86cff66f0cffdf347226c32e1ba8
Shivam Garg <shivamcp2694@gmail.com>
2025-03-25 20:06:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/neo4j-visualization/'
+  base: '/neo4j-visualization'
   
 })
